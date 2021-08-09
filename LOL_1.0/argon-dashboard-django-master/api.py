20c463f6ad3e6d8bac1d28321126f8a2d8d34931
K2lFrankenstein <k2leagle@gmail.com>
2021-08-10 00:24:28 +0530

    using Cmd

--- LOL_1.0/argon-dashboard-django-master/api.py
+++ LOL_1.0/argon-dashboard-django-master/api.py
@@ -1,6 +1,25 @@
 import quandl 
 import pandas as pd
+from github import Github
+# quandl config
 quandl.ApiConfig.api_key = 'RATUYxBwY63sVF7F8PBQ'
 dateparse = lambda dates: pd.datetime.strptime(dates, '%Y-%m-%d')
-data = quandl.get('BSE/BOM500180')
-print(data)
+
+#github config
+
+g = Github("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")
+repo = g.get_user().get_repo('test')
+
+# fetching data from api
+data = quandl.get('BSE/BOM500057')
+df = pd.DataFrame(data, columns = ['Open','High','Low','Close'])
+df.to_json(r'D:\psudo_desktop\KSI\data.json',orient="table")
+
+# Prepare files to upload to GitHub
+files = ['D:\psudo_desktop\KSI\data.json']
+
+# Make a commit and push
+commit_message =
+repo.commit(files)
+origin = repo.remote('origin')
+origin.push()
---
+++ LOL_1.0/argon-dashboard-django-master/samplegit.py
@@ -0,0 +1,13 @@
+import subprocess
+
+user = 'K2lFrankenstein'
+password = 'Stfu@32057'
+
+cmd = "D:\psudo_desktop\KSI\sample\data.json"
+returned_value = subprocess.call(cmd, shell=True)  # returns the exit code in unix
+
+cmd = "git add ." 
+subprocess.call(cmd, shell=True)
+
+cmd = 'git commit -m " using Cmd"'
+subprocess.call(cmd, shell=True)
