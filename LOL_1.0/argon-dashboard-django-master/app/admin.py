c0435735eb2d5931525a7679eb77a847cfc1000b
K2lFrankenstein <k2leagle@gmail.com>
2021-08-24 13:09:05 +0530

    modelling complete

--- LOL_1.0/argon-dashboard-django-master/app/admin.py
+++ LOL_1.0/argon-dashboard-django-master/app/admin.py
@@ -4,5 +4,7 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django.contrib import admin
-
+from app.models import Products
 # Register your models here.
+
+admin.site.register(Products)
--- LOL_1.0/argon-dashboard-django-master/samplegit.py
+++ LOL_1.0/argon-dashboard-django-master/samplegit.py
@@ -7,8 +7,13 @@ cmd = "D:\psudo_desktop\KSI\sample\data.json"
 returned_value = subprocess.call(cmd, shell=True)  # returns the exit code in unix
 
 cmd= "git clone https://github.com/K2lFrankenstein/test.git"
+subprocess.call(cmd, shell=True)
+
 cmd = "git add ." 
 subprocess.call(cmd, shell=True)
 
 cmd = 'git commit -m " using Cmd"'
+subprocess.call(cmd, shell=True)
+
+cmd = "git push"
 subprocess.call(cmd, shell=True)
